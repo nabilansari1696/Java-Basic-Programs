6edb2f8181b62f1308d10428b28c032bd060eeeb
Nabil Ansari <nabil.vo379@gmail.com>
2021-02-11 15:56:35 +0530

    commit

---
+++ src/Basics/ReverseString.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Basics;
+
+public class ReverseString {
+    
+    public static void main(String[] args){
+    String str = "ABCD";
+    String rev ;
+    
+    int len = str.length();
+    
+    for(int i = len - 1; i >= 0; i--){
+    rev = rev + str.charAt(i);
+}
+    System.out.println(rev);
+    
+}
+}
